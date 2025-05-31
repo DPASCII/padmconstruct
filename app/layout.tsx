2488e39d102897513d145f08a66bde00284f6249
David Pascual <davidcpascual@gmail.com>
2025-05-31 16:41:07 +0800

    remove FOUC fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,20 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import StyledComponentsRegistry from '../lib/registry';
-
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "PADM",
-  description: "PADM Construction",
+  title: 'PADM',
+  description: 'PADM Construction',
 };
 
 export default function RootLayout({
@@ -26,14 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <StyledComponentsRegistry>
-        <body
+      <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-      </body></StyledComponentsRegistry>
-      
+      </body>
     </html>
   );
 }
-
