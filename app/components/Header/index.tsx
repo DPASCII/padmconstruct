338bec5716a2c0ce3840012aecdd76bc003bfd7a
DPASCII <davidcpascual@gmail.com>
2025-06-28 16:45:21 +0800

    remove next Image

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Image from 'next/image';
 import React, { useCallback, useEffect, useState } from 'react';
 import styled, { useTheme } from 'styled-components';
 import Hamburger from './components/hamburger';
