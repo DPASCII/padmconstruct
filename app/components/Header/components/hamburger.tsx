f21ea816ef1170393775f0e178f27fe3cdbf27f6
DPASCII <davidcpascual@gmail.com>
2025-06-28 15:53:11 +0800

    fix hamburger

--- app/components/Header/components/hamburger.tsx
+++ app/components/Header/components/hamburger.tsx
@@ -13,35 +13,46 @@ const Bun = styled.div`
   flex-flow: column nowrap;
   border-radius: 6px;
   z-index: 10;
+
   cursor: pointer;
 `;
 
 const Burger = styled.div<{
   $isOpen?: boolean;
   $index?: number;
-  $burgerColor?: string;
 }>`
-  width: ${({ $isOpen, $index }) => ($isOpen && $index === 2 ? '0' : '2.5rem')};
+  width: 2.5rem;
   height: 0.4em;
   border-radius: 10px;
   background: rgb(${getThemeColors('secondary')});
   transform-origin: 1px;
   transition: all 0.3s linear;
   margin: auto;
-  transform: ${({ $isOpen, $index }) =>
-    $isOpen &&
-    ($index === 1
-      ? 'rotate(45deg) translateX(5%)'
-      : $index === 2
-        ? 'translateX(0%)'
-        : 'rotate(-45deg) translateX(5%)')};
 `;
 
 export const Hamburger = ({ isOpen }: { isOpen?: boolean }) => (
   <Bun>
-    <Burger $isOpen={isOpen} $index={1} />
-    <Burger $isOpen={isOpen} $index={2} />
-    <Burger $isOpen={isOpen} $index={3} />
+    <Burger
+      $isOpen={isOpen}
+      style={
+        isOpen
+          ? { transform: 'rotate(45deg) translateX(5%)' }
+          : { transform: 'rotate(0deg)' }
+      }
+    />
+    <Burger
+      $isOpen={isOpen}
+      style={isOpen ? { width: '0' } : { transform: '2.5rem' }}
+    />
+    <Burger
+      $isOpen={isOpen}
+      $index={3}
+      style={
+        isOpen
+          ? { transform: 'rotate(-45deg) translateX(5%)' }
+          : { transform: 'rotate(0deg)' }
+      }
+    />
   </Bun>
 );
 
