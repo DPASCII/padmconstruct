b0c3347de4889fe695603a27db0e557ad7fabf20
DPASCII <davidcpascual@gmail.com>
2025-06-28 21:32:28 +0800

    add Jumbotron

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,31 @@
+'use client';
+
+import Button from './components/Button';
 import ComingSoon from './components/coming-soon';
+import Jumbotron from './components/Jumbotron';
 
 const HomePage = () => {
-  return <ComingSoon />;
+  return (
+    <div>
+      <Jumbotron
+        images={[
+          '/assets/1.jpg',
+          '/assets/2.jpg',
+          '/assets/3.jpg',
+          '/assets/4.jpg',
+          '/assets/5.jpg',
+          '/assets/6.jpg',
+          '/assets/7.jpg',
+          '/assets/8.jpg',
+        ]}
+        title="Build Better. Build with Confidence."
+        subtitle={
+          'We turn your vision into reality \non time, on budget, and built to last'
+        }
+        children={<Button label="Our Projects" style={{ height: '50px' }} />}
+      />
+    </div>
+  );
 };
 
 export default HomePage;
