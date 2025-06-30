164669cfac6427e9ad145e14d7b8e6966f38fbbd
DPASCII <davidcpascual@gmail.com>
2025-06-30 16:08:02 +0800

    fix photos

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -8,26 +8,26 @@ const Projects = ({}: {}) => {
   const projectList = [
     {
       text: 'Construction of Stone Masonry Works',
-      src: '/assets/1.jpg',
+      src: '/assets/6.jpg',
       alt: 'alt',
     },
     {
       text: 'Installation of Tiles for PDIC Building',
-      src: '/assets/2.jpg',
+      src: '/assets/8.jpg',
       alt: 'alt',
     },
     { text: 'Prefabricated Container Van', src: '/assets/3.jpg', alt: 'alt' },
-    { text: 'Structural Steel Works', src: '/assets/4.jpg', alt: 'alt' },
+    { text: 'Structural Steel Works', src: '/assets/7.jpg', alt: 'alt' },
     {
       text: 'Supply and Installation of Phenolic Board Protection',
       src: '/assets/5.jpg',
       alt: 'alt',
     },
-    { text: '3-Unit Residential Apartment', src: '/assets/6.jpg', alt: 'alt' },
-    { text: 'Coffee Shop Renovation', src: '/assets/7.jpg', alt: 'alt' },
+    { text: '3-Unit Residential Apartment', src: '/assets/1.jpg', alt: 'alt' },
+    { text: 'Coffee Shop Renovation', src: '/assets/2.jpg', alt: 'alt' },
     {
       text: 'Kyu-Kyu Ramen Restaurant Renovation',
-      src: '/assets/8.jpg',
+      src: '/assets/4.jpg',
       alt: 'alt',
     },
   ];
