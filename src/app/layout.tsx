575e2348a6a646372222663d581d3e5304f1ab7a
DPASCII <davidcpascual@gmail.com>
2025-07-01 11:57:52 +0800

    change icon in tab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,12 @@ import Header from '../components/header';
 import { FooterLayout } from '../components/footer';
 import BottomContent from '../components/footer/components/bottom-content';
 import ContentList from '../components/footer/components/content-list';
+import Head from 'next/head';
 
 export const metadata: Metadata = {
-  title: 'PADM',
+  title: 'PADM Construct',
   description: 'PADM Construction',
+  icons: '/assets/small PADM.png',
 };
 
 export default function RootLayout({
@@ -18,58 +20,63 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        <div className="site">
-          <StyledComponentsRegistry>
-            <ThemeRegistry>
-              <Header
-                logo="/assets/PADM Logo.png"
-                height={80}
-                pages={[
-                  { item: 'Home', link: '/' },
-                  { item: 'Our Projects', link: '/projects' },
-                ]}
-              />
-              {children}
-              <FooterLayout
-                children={
-                  <>
-                    <ContentList
-                      title="Contact Us"
-                      itemlist={[
-                        {
-                          item: 'Address:',
-                          subitem:
-                            '66 Istanbul Street, BF Midwest, BF Homes, Parañaque City',
-                        },
-                        {
-                          item: 'Phone:',
-                          subitem: '09171841267',
-                          link: 'tel:+639171841267',
-                        },
-                        {
-                          item: 'Email:',
-                          subitem: 'padmconstruct@gmail.com',
-                          link: 'mailto:padmconstruct@gmail.com',
-                        },
-                      ]}
-                      style={{ justifySelf: 'left' }}
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <html lang="en">
+        <body>
+          <div className="site">
+            <StyledComponentsRegistry>
+              <ThemeRegistry>
+                <Header
+                  logo="/assets/PADM Logo.png"
+                  height={80}
+                  pages={[
+                    { item: 'Home', link: '/' },
+                    { item: 'Our Projects', link: '/projects' },
+                  ]}
+                />
+                {children}
+                <FooterLayout
+                  children={
+                    <>
+                      <ContentList
+                        title="Contact Us"
+                        itemlist={[
+                          {
+                            item: 'Address:',
+                            subitem:
+                              '66 Istanbul Street, BF Midwest, BF Homes, Parañaque City',
+                          },
+                          {
+                            item: 'Phone:',
+                            subitem: '09171841267',
+                            link: 'tel:+639171841267',
+                          },
+                          {
+                            item: 'Email:',
+                            subitem: 'padmconstruct@gmail.com',
+                            link: 'mailto:padmconstruct@gmail.com',
+                          },
+                        ]}
+                        style={{ justifySelf: 'left' }}
+                      />
+                    </>
+                  }
+                  bottomContent={
+                    <BottomContent
+                      companyName="PADMConstruct"
+                      smallLogo="/assets/small PADM.png"
+                      mailTo="padmconstruct@gmail.com"
                     />
-                  </>
-                }
-                bottomContent={
-                  <BottomContent
-                    companyName="PADMConstruct"
-                    smallLogo="/assets/small PADM.png"
-                    mailTo="padmconstruct@gmail.com"
-                  />
-                }
-              />
-            </ThemeRegistry>
-          </StyledComponentsRegistry>
-        </div>
-      </body>
-    </html>
+                  }
+                />
+              </ThemeRegistry>
+            </StyledComponentsRegistry>
+          </div>
+        </body>
+      </html>
+    </>
   );
 }
